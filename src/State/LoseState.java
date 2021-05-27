f56a957da912f67fa0966bfefb414d8b71568bec
phuctv302 <phuc.tv194354@sis.hust.edu.vn>
2021-05-27 22:52:08 +0700

    fix nhac lan 2

--- src/State/LoseState.java
+++ src/State/LoseState.java
@@ -22,6 +22,7 @@ public class LoseState extends State{
         uiManager.addObject(new UIImageButton(300,100, 200, 71, Asset.buttonRestart, new ClickListener(){
             public void onClick(){
                 handler.getMouseManager().setUiManager(null);
+                handler.getAudioPlayer().playMusic("gaoMusic.wav");
                 State.setState(new MenuState(handler));
             }
         }));
